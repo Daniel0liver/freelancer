243a4676687eadfe047454678cc3eac376cb5b6f
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-02-13 22:01:38 -0300

    chore: remove import animated

--- src/components/ScrollView/index.tsx
+++ src/components/ScrollView/index.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import { ScrollView, Animated, ScrollViewProps } from 'react-native';
+import { ScrollView, ScrollViewProps } from 'react-native';
 
 interface IScrollViewProps {
   children: ScrollViewProps;
 }
 
-const AnimatedScrollView = Animated.createAnimatedComponent(ScrollView);
-
 export default function CustomScrollView({ children }: IScrollViewProps) {
   return (
-    <AnimatedScrollView
-      scrollEventThrottle={1}
-      showsVerticalScrollIndicator={false}>
+    <ScrollView scrollEventThrottle={1} showsVerticalScrollIndicator={false}>
       {children}
-    </AnimatedScrollView>
+    </ScrollView>
   );
 }
