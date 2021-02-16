e130805da7dc0382b78b3e81e51e3e5c94f2d9cd
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-02-16 20:09:44 -0300

    chore: resolve merge conflits

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import { Stack } from './navigation';
 
-import Header from './components/Header'
+import Header from './components/Header';
 
 import OnBoard from './pages/OnBoard';
 import Dashboard from './pages/Dashboard';
@@ -12,18 +12,19 @@ import Settings from './pages/Settings';
 
 import { Button, Icon } from './components/Header/styles';
 
-
 export default function Routes() {
   const theme = useTheme();
   const navigationRef = useRef(null);
 
   return (
     <NavigationContainer ref={navigationRef}>
-      <Stack.Navigator initialRouteName="OnBoard" screenOptions={{
-        header: ({ scene, navigation, previous }) => (
-          <Header scene={scene} navigation={navigation} previous={previous} />
-        )
-      }}>
+      <Stack.Navigator
+        initialRouteName="OnBoard"
+        screenOptions={{
+          header: ({ scene, navigation, previous }) => (
+            <Header scene={scene} navigation={navigation} previous={previous} />
+          ),,
+        }}>
         <Stack.Screen
           name="OnBoard"
           component={OnBoard}
@@ -37,10 +38,15 @@ export default function Routes() {
           options={{
             title: 'Hi, Daniel Oliveira',
             headerRight: () => (
-              <Button onPress={() => navigationRef.current?.navigate('Settings')}>
-                <Icon name="settings" size={20} color={theme.palette.primary.contrastText} />
+              <Button
+                onPress={() => navigationRef.current?.navigate('Settings')}>
+                <Icon
+                  name="settings"
+                  size={20}
+                  color={theme.palette.primary.contrastText}
+                />
               </Button>
-            )
+            ),
           }}
         />
         <Stack.Screen
