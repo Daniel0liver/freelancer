18d4752e145992e48cbc6d3b4f37dbdcb7ed8994
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-03-07 20:34:55 -0300

    chore: improve text input and dispatch action signin

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
+import { Alert } from 'react-native';
+import { useDispatch } from 'react-redux';
 import { KeyboardAvoidingView, Platform } from 'react-native';
+
+import { signInResquest } from '../../store/modules/auth/actions';
 
 import Input from '../../components/TextInput';
 
 import { SafeAreaView, Title, Button, ArrowLeftIcon } from './styles';
 
 export default function SignIn() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState('daniel@dev.com');
+  const [password, setPassword] = useState('123456');
+
+  const handleSubmit = () => {
+    if (email.length && password.length) {
+      dispatch(signInResquest({ email, password }));
+    } else {
+      Alert.alert('Atenção', 'Preencha os campos corretamente.');
+    }
+  };
 
   return (
     <SafeAreaView>
@@ -18,16 +32,28 @@ export default function SignIn() {
           ios: 'padding',
           android: 'height',
         })}>
-        <Input label="Email" value={email} onChangeText={setEmail} />
+        <Input
+          label="Email"
+          value={email}
+          onChangeText={setEmail}
+          autoFocus
+          returnKeyType="next"
+          autoCapitalize="none"
+          textContentType="emailAddress"
+          keyboardType="email-address"
+        />
+
         <Input
           label="Password"
           value={password}
           onChangeText={setPassword}
           secureTextEntry
+          returnKeyType="done"
+          onSubmitEditing={handleSubmit}
         />
       </KeyboardAvoidingView>
 
-      <Button>
+      <Button onPress={handleSubmit}>
         <ArrowLeftIcon />
       </Button>
     </SafeAreaView>
